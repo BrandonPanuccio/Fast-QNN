5304a1595a6faf27746c18afab0bb9eb83bbeefe
BrandonPanuccio <bpanucci@villanova.edu>
2024-12-05 13:19:35 -0500

    accuracy rates

--- torch_pretrained_test.py
+++ torch_pretrained_test.py
@@ -17,7 +17,7 @@ def initialize_weights(model):
     for m in model.modules():
         if isinstance(m, nn.Conv2d):
             nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
-            if m.bias is not None and m.bias.data is not None:
+            if hasattr(m, 'bias') and m.bias is not None:
                 nn.init.constant_(m.bias, 0)
         elif isinstance(m, nn.BatchNorm2d):
             nn.init.constant_(m.weight, 1)
